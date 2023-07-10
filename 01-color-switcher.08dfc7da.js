14fdb83e82472ae91a435e0cb493695dd811efe6
sirLightRain <34480855+sirLightRain@users.noreply.github.com>
2023-07-10 13:32:58 +0000

    Deploying to gh-pages from @ sirLightRain/goit-js-hw-09@49ad2a2977ef684e75e94f8467b6158178bb37db 🚀

--- 01-color-switcher.08dfc7da.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let n;t.addEventListener("click",(function(){t.disabled=!0,n=setInterval((function(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1}));
-//# sourceMappingURL=01-color-switcher.08dfc7da.js.map
